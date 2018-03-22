c149757a36d5555bb117f25f7ff7ac6b70b998c6
dritan92 <saidahoxha@hotmail.com>
2018-03-23 09:38:05 +1100

    First commit

---
+++ Library/Controllers/CatalogController.cs
@@ -0,0 +1,42 @@
+﻿using Library.Models.Catalog;
+using LibraryData;
+using LibraryData.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Controllers
+{
+    public class CatalogController
+    {
+    
+        private ILibraryAsset _assets;
+        public CatalogController(ILibraryAsset assets)
+        {
+            _assets = assets;
+        }
+        public IActionResult Index()
+        {
+            var assetModels = _assets.GetAll();
+            var listingResult = assetModels
+                .Select(result => new AssetIndexListingModels
+                {
+                    Id = result.Id,
+                    ImageUrl = result.imageURL,
+                    AuthorOrDirector = _assets.GetAuthorOrDirector(result.Id),
+                    DeweyCallNumber = _assets.GetDeweyIndex(result.Id),
+                    Title = result.Title,
+                    Type = _assets.GetType(result.Id)
+                });
+            var model = new AssetIndexModel()
+            {
+                Assets = listingResult
+            };
+            return View(model);
+        }
+
+    }
+}
+
---
+++ LibraryData/Models/Checkout.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LibraryData.Models
+{
+    public class Checkout
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public LibraryAsset LibraryAsset { get; set; }
+
+        [Required]
+        public LibraryCard LibraryCard { get; set; }
+
+        [Required]
+        public DateTime Until { get; set; }
+
+        [Required]
+        public DateTime Since { get; set; }
+
+
+    }
+}
